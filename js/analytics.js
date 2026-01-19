d4c1836b979d34d27b4ef9921fb398faad24dafb
BJ Beier <bjbeier@gmail.com>
2026-01-18 20:08:27 -0500

    feat: Implement a theme-aware cookie consent banner to conditionally load Google Analytics.

--- js/analytics.js
+++ js/analytics.js
@@ -1,4 +1,11 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'G-5R4EJK544M');
+window.loadTracking = function () {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-5R4EJK544M";
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-5R4EJK544M');
+};
